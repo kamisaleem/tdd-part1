f90a6383e84c8560e0f03f2a6cb9f1573a1e61cf
Muhammad Kamran Saleem <kamran.saleem@globalnorthstar.com>
2014-12-28 15:30:41 +0500

    Chapter 3. Equality for All

--- TheMoneyExample.java
+++ TheMoneyExample.java
@@ -17,6 +17,12 @@ public class TheMoneyExample {
 		assertEquals(10, product.amount);
 		product= five.times(3);
 		assertEquals(15, product.amount);
+		testEquality();
+	}
+	
+	public void testEquality() {
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+		assertFalse(new Dollar(5).equals(new Dollar(6)));
 	}
 	
 	class Dollar {
@@ -30,6 +36,11 @@ public class TheMoneyExample {
 		Dollar times(int multiplier) {
 			return new Dollar (amount * multiplier);
 		}
+		
+		public boolean equals(Object object) {
+			Dollar dollar = (Dollar) object;
+			return amount == dollar.amount;
+		}
+		
 	}
-
 }
