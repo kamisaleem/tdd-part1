e553305007af11e644cb2f5857d92af7596a3723
Muhammad Kamran Saleem <kamran.saleem@globalnorthstar.com>
2014-12-28 16:00:47 +0500

    Chapter 5. Franc-ly Speaking

--- TheMoneyExample.java
+++ TheMoneyExample.java
@@ -23,6 +23,13 @@ public class TheMoneyExample {
 		assertFalse(new Dollar(5).equals(new Dollar(6)));
 	}
 	
+	@Test
+	public void testFrancMultiplication() {
+		Franc five = new Franc(5);
+		assertEquals(new Franc(10), five.times(2));
+		assertEquals(new Franc(15), five.times(3));
+	}
+	
 	class Dollar {
 		
 		private int amount;
@@ -38,6 +45,25 @@ public class TheMoneyExample {
 		public boolean equals(Object object) {
 			Dollar dollar = (Dollar) object;
 			return amount == dollar.amount;
+		}
+		
+	}
+	
+	class Franc {
+		
+		private int amount;
+		
+		Franc(int amount) {
+			this.amount = amount;
+		}
+		
+		Franc times(int multiplier) {
+			return new Franc (amount * multiplier);
+		}
+		
+		public boolean equals(Object object) {
+			Franc franc = (Franc) object;
+			return amount == franc.amount;
 		}
 		
 	}
