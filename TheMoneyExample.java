322fb667e6f7ccf23e7b68953475a06c31735c61
Muhammad Kamran Saleem <kamran.saleem@globalnorthstar.com>
2014-12-28 23:06:59 +0500

    Chapter 8. Makin' Objects

--- TheMoneyExample.java
+++ TheMoneyExample.java
@@ -12,58 +12,69 @@ public class TheMoneyExample {
 
 	@Test
 	public void testMultiplication() {
-		Dollar five = new Dollar(5);
-		assertEquals(new Dollar(10), five.times(2));
-		assertEquals(new Dollar(15), five.times(3));
+		Money five = Money.dollar(5);
+		assertEquals(Money.dollar(10), five.times(2));
+		assertEquals(Money.dollar(15), five.times(3));
 	}	
 
-	@Test
-	public void testFrancMultiplication() {
-		Franc five = new Franc(5);
-		assertEquals(new Franc(10), five.times(2));
-		assertEquals(new Franc(15), five.times(3));
-	}
-	
 	@Test
 	public void testEquality() {
-		assertTrue(new Dollar(5).equals(new Dollar(5)));
-		assertFalse(new Dollar(5).equals(new Dollar(6)));
-		assertTrue(new Franc(5).equals(new Franc(5)));
-		assertFalse(new Franc(5).equals(new Franc(6)));
-		assertFalse(new Franc(5).equals(new Dollar(5)));
-	}	
-	
-	class Money {
-		protected int amount;
-		
-		public boolean equals(Object object) {
-			Money money = (Money) object;
-			return amount == money.amount
-					&& getClass().equals(money.getClass());
-		}
+		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+		assertTrue(Money.franc(5).equals(Money.franc(5)));
+		assertFalse(Money.franc(5).equals(Money.franc(6)));
+		assertFalse(Money.franc(5).equals(Money.dollar(5)));
 	}
 	
-	class Dollar extends Money {
-				
-		Dollar(int amount) {
-			this.amount = amount;
-		}
-		
-		Dollar times(int multiplier) {
-			return new Dollar (amount * multiplier);
-		}
-		
+	@Test
+	public void testFrancMultiplication() {
+		Money five = Money.franc(5);
+		assertEquals(Money.franc(10), five.times(2));
+		assertEquals(Money.franc(15), five.times(3));
 	}
-	
-	class Franc extends Money {
-				
-		Franc(int amount) {
-			this.amount = amount;
-		}
-		
-		Franc times(int multiplier) {
-			return new Franc (amount * multiplier);
-		}
+}
 
+abstract class Money {
+	
+	protected int amount;
+	
+	abstract Money times(int multiplier);
+	
+	static Dollar dollar(int amount) {
+		return new Dollar(amount);
 	}
+	
+	static Franc franc(int amount) {
+		return new Franc(amount);
+	}
+	
+	public boolean equals(Object object) {
+		Money money = (Money) object;
+		return amount == money.amount
+				&& getClass().equals(money.getClass());
+	}
+}
+
+class Dollar extends Money {
+	
+	Dollar(int amount) {
+		this.amount = amount;
+	}
+	
+	Money times(int multiplier) {
+		return new Dollar (amount * multiplier);
+	}
+	
+}
+
+class Franc extends Money {
+			
+	Franc(int amount) {
+		this.amount = amount;
+	}
+	
+	Money times(int multiplier) {
+		return new Franc (amount * multiplier);
+	}
+
 }
