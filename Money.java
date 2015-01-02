aebbc81a777307d4f652fa551b4388f73f495285
Muhammad Kamran Saleem <kamran.saleem@globalnorthstar.com>
2015-01-02 12:02:02 +0500

    Chapter 15. Mixed Currencies

--- Money.java
+++ Money.java
@@ -18,7 +18,7 @@ public class Money implements Expression {
 		return new Franc(amount, "CHF");
 	}
 
-	Money times(int multiplier) {
+	Expression times(int multiplier) {
 		return new Money(amount * multiplier, currency);
 	}
 	
@@ -28,7 +28,7 @@ public class Money implements Expression {
 				&& currency().equals(money.currency());
 	}
 	
-	Expression plus(Money addend) {
+	public Expression plus(Expression addend) {
 		return new Sum(this, addend);
 	}
 	
